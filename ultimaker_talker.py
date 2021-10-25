fc9a29ade5ae27ea9fb4dc25912efe307575e30e
Adrian Falke <92855802+ad0409@users.noreply.github.com>
2021-10-25 14:20:30 +0200

    Update and rename talker_ultimaker.py to ultimaker_talker.py

--- ultimaker_talker.py
+++ ultimaker_talker.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 
 import rospy
@@ -6,8 +5,8 @@ from std_msgs.msg import String
 
 
 def talker():
-    pub = rospy.Publisher('chatter_ultimaker', String, queue_size=10)
-    rospy.init_node('talker_ultimaker', anonymous=True)
+    pub = rospy.Publisher('ultimaker_chatter', String, queue_size=10)
+    rospy.init_node('ultimaker_talker', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         user_input = input("Enter Input: ").strip()
