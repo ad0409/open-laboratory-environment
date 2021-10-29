bc5d190a9438f6ebb5e7f0bf76371781313e256c
Adrian Falke <92855802+ad0409@users.noreply.github.com>
2021-10-29 17:14:14 +0200

    Create ultimaker_support_functions.py

---
+++ ultimaker_support_functions.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import serial
+import time
+
+
+def move_printer_x(mm_wanted_x):  # currently not working with negative ints
+    print('mm_wanted_x')
+    print(mm_wanted_x)
+    raw_command_x = str.encode('G0 X ' + str(mm_wanted_x) + ' F0\n')
+    return raw_command_x
+
+
+def move_printer_y(mm_wanted_y):  # currently not working with negative ints
+    print('mm_wanted_y')
+    print(mm_wanted_y)
+    raw_command_y = str.encode('G0 X ' + str(mm_wanted_y) + ' F0\n')
+    return raw_command_y
+
+
+def move_printer_z(mm_wanted_z):  # currently not working with negative ints
+    print('mm_wanted_z')
+    print(mm_wanted_z)
+    raw_command_z = str.encode('G0 X ' + str(mm_wanted_z) + ' F0\n')
+    return raw_command_z
+
+
+def initialize_serial_port():
+    ser = serial.Serial()
+    ser.port = '/dev/ttyACM0'
+    ser.baudrate = 250000
+    # ser.bytesize =
+    ser.parity = serial.PARITY_EVEN
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.timeout = 1
+    ser.xonxoff = 0
+    ser.rtscts = 1
+    ser.dsrdtr = 1
+    print('serial is open: ' + str(ser.is_open))
+
+    if ser.is_open:
+        print('Serial open.. continue')
+    else:
+        print('Serial closed... open it.')
+        ser.open()  # check ser.is_open to see if serial is open
+        print('Wait 1s')
+        time.sleep(1)
