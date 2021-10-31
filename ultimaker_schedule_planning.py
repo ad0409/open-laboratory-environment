a37bb7f1acce8599e0fbafbcc41a336d61ebc6e9
Adrian Falke <92855802+ad0409@users.noreply.github.com>
2021-10-31 15:24:41 +0100

    Update and rename ultimaker_path_planning.py to ultimaker_schedule_planning.py

--- ultimaker_schedule_planning.py
+++ ultimaker_schedule_planning.py
@@ -6,7 +6,34 @@ import ultimaker_support_functions as usf
 ser = serial.Serial()  # define class
 
 
-def path_1():
+def schedule_1():
+    usf.open_serial_port()  # open serial port
+    print('Printer online')
+    # print('inline bytes waiting: ' + str(ser.in_waiting))
+    print('Flushing Data')
+    ser.flush()  # flushing data
+    time.sleep(1)
+
+    mm_wanted_x = 100
+    raw_command_x = usf.move_printer_x(mm_wanted_x)  # moving x-axis
+    print('RAW Command: ' + raw_command_x)
+    ser.write(raw_command_x)
+
+    mm_wanted_y = 100
+    raw_command_y = usf.move_printer_y(mm_wanted_y)  # moving y-axis
+    print('RAW Command: ' + raw_command_y)
+    ser.write(raw_command_y)
+
+    # mm_wanted_z = 100
+    # raw_command_z = usf.move_printer_z(mm_wanted_z)  # moving z-axis
+    # print('RAW Command: ' + raw_command_z)
+    # ser.write(raw_command_z)
+
+    usf.homing_printer()  # homing printer
+    usf.close_serial_port()  # close serial port
+
+
+def schedule_2():
     usf.open_serial_port()  # open serial port
     print('Printer online')
     # print('inline bytes waiting: ' + str(ser.in_waiting))
