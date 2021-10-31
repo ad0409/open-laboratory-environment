ebd8447e0c2af72fa64666c114c7bef1403c7116
Adrian Falke <92855802+ad0409@users.noreply.github.com>
2021-10-31 12:34:20 +0100

    Update ultimaker_listener.py
    adding support functions within callback() method; making path planning easier

--- ultimaker_listener.py
+++ ultimaker_listener.py
@@ -4,89 +4,45 @@ import rospy
 from std_msgs.msg import String
 import serial
 import time
+import ultimaker_support_functions as usf
 
-
-# def move_printer(x_mm_wanted):      # currently not working with negative ints
-#     print('x_mm_wanted')
-#     print(x_mm_wanted)
-#     raw_command = str.encode('G0 X ' + str(x_mm_wanted) + ' F0\n')
-#     # raw_command = str.encode('G0 X 100 ' + 'F0\n')
-#     return raw_command
+ser = serial.Serial()  # define class
 
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id())  # + "  I heard %s", data.data)
     print('I heard ' + data.data)
-    my_dict = {'homing': 'G28\n',
-               'run': 'do',
-               'taille': 1.75}
+    # my_dict = {'homing': 'G28\n',
+    #            'run': 'do',
+    #            'taille': 1.75}
 
-    ser = serial.Serial()
-    ser.port = '/dev/ttyACM0'
-    ser.baudrate = 250000
-    # ser.bytesize =
-    ser.parity = serial.PARITY_EVEN
-    ser.stopbits = serial.STOPBITS_ONE
-    ser.timeout = 1
-    ser.xonxoff = 0
-    ser.rtscts = 1
-    ser.dsrdtr = 1
-    print('serial is open: ' + str(ser.is_open))
-    print('serial is open: ' + str(ser.is_open))
-
-    if ser.is_open:
-        print('Serial open.. continue')
-    else:
-        print('Serial closed... open it.')
-        ser.open()  # check ser.is_open to see if serial is open
-        print('Wait 1s')
-        time.sleep(1)
-
+    # ser = serial.Serial()
+    usf.open_serial_port()  # open serial port
     print('Printer online')
     # print('inline bytes waiting: ' + str(ser.in_waiting))
     print('Flushing Data')
-    ser.flush()
+    ser.flush()  # flushing data
     time.sleep(1)
-    # print('inline bytes waiting: ' + str(ser.in_waiting))
-    # print('Do some magic')
-    print('Home:')
-    # ser.write(str.encode(my_dict[data.data]))
-    print('Wait 1s')
-    time.sleep(1)
-    # print('Move X!')
-    # ser.write(bytes(b'G0 X100 F0\n'))
-    # ser.write(bytes(b'G91'))
-    # print('Move X!')
 
-    # x_mm_wanted = -100
-    # raw_command = move_printer(x_mm_wanted)
-    # print('RAW Command:')
-    # print(raw_command)
-    # ser.write(raw_command)
+    mm_wanted_x = 100
+    raw_command_x = usf.move_printer_x(mm_wanted_x)  # moving x-axis
+    print('RAW Command:')
+    print(raw_command_x)
+    ser.write(raw_command_x)
 
-    # print(move_printer(bytes(x_mm_wanted)))
-    # ser.write(str.encode(my_dict[data.data]))
-    # print('Moving!')
-    # print('Call Read positions:')
-    # ser.write(bytes(b'M114\n'))
-    # print('Read serial:')
-    # print(ser.read())
-    # print('Move Y!')
-    # ser.write(bytes(b'G0 Y100.0 F0\n'))
-    # # ser.write(bytes(b'G90'))
-    # print('Sleep for 1s')
-    # time.sleep(1)
-    # print('Move -X!')
-    # ser.write(bytes(b'G0 X-100.0 F0\n'))
-    # print('Home again:')
-    # # ser.write(bytes(b'G28\n'))
-    # ser.write(str.encode(my_dict[data.data]))
-    # print('Wake up')
-    # print('inline bytes waiting: ' + str(ser.in_waiting))
-    # time.sleep(1)
-    # print('Sleep for 1s')
-    # time.sleep(1)
-    # print('inline bytes waiting: ' + str(ser.in_waiting))
+    mm_wanted_y = 100
+    raw_command_y = usf.move_printer_y(mm_wanted_y)  # moving y-axis
+    print('RAW Command:')
+    print(raw_command_y)
+    ser.write(raw_command_y)
+
+    mm_wanted_z = 100
+    raw_command_z = usf.move_printer_z(mm_wanted_z)  # moving z-axis
+    print('RAW Command:')
+    print(raw_command_z)
+    ser.write(raw_command_z)
+
+    usf.close_serial_port()  # close serial port
 
 
 def listener():
@@ -106,8 +62,43 @@ if __name__ == '__main__':
     listener()
     # callback('run')
 
+# SNIPPETS
 # python3 -m serial.tools.list_ports
 # homing = 'G28\n'  # better solution to encode string to bytes
 # ser.write(str.encode(my_dict[data.data]))
-# raw_command = move_printer(bytes([x_mm_wanted]))
-# raw_command = move_printer(x_mm_wanted.to_bytes(2, byteorder='little'))
+# raw_command = move_printer(bytes([mm_wanted_x]))
+# raw_command = move_printer(mm_wanted_x.to_bytes(2, byteorder='little'))
+# print('inline bytes waiting: ' + str(ser.in_waiting))
+# print('Do some magic')
+# print('Home:')
+# ser.write(str.encode(my_dict[data.data]))
+# print('Wait 1s')
+# time.sleep(1)
+# print('Move X!')
+# ser.write(bytes(b'G0 X100 F0\n'))
+# ser.write(bytes(b'G91'))
+# print('Move X!')
+# print(move_printer(bytes(mm_wanted_x)))
+# ser.write(str.encode(my_dict[data.data]))
+# print('Moving!')
+# print('Call Read positions:')
+# ser.write(bytes(b'M114\n'))
+# print('Read serial:')
+# print(ser.read())
+# print('Move Y!')
+# ser.write(bytes(b'G0 Y100.0 F0\n'))
+# # ser.write(bytes(b'G90'))
+# print('Sleep for 1s')
+# time.sleep(1)
+# print('Move -X!')
+# ser.write(bytes(b'G0 X-100.0 F0\n'))
+# print('Home again:')
+# # ser.write(bytes(b'G28\n'))
+# ser.write(str.encode(my_dict[data.data]))
+# print('Wake up')
+# print('inline bytes waiting: ' + str(ser.in_waiting))
+# time.sleep(1)
+# print('Sleep for 1s')
+# time.sleep(1)
+# print('inline bytes waiting: ' + str(ser.in_waiting))
+# raw_command = str.encode('G0 X 100 ' + 'F0\n')
