b0c4fc85c5bfd575be2cd6ce5979c3db0ea977de
Adrian Falke <92855802+ad0409@users.noreply.github.com>
2021-11-04 21:49:11 +0100

    Update ultimaker_listener.py

--- ultimaker_listener.py
+++ ultimaker_listener.py
@@ -4,6 +4,7 @@ import rospy
 from std_msgs.msg import String
 import serial
 import ultimaker_schedule_planning as usp
+import ultimaker_support_functions as usf
 
 ser = serial.Serial()  # define class
 
@@ -12,21 +13,23 @@ def callback(data):  # logging user input data, defining path planning
     rospy.loginfo(rospy.get_caller_id())
     print('I heard ' + data.data)
 
-    if data.data == 'schedule_1':  # check user input against predefined schedule matching words
+    if data.data == 's1':  # check user input against predefined schedule matching words
         usp.schedule_1()
-    elif data.data == 'schedule_2':
-        usp.schedule_2()
+    # elif data.data == 'schedule_2':
+    #     usp.schedule_2()
+    elif data.data == 'stop':
+        usf.stop()
     else:
         print('Wrong schedule name.')
 
 
 def listener():
-    rospy.init_node('ultimaker_listener', anonymous=True)
-    rospy.Subscriber('ultimaker_chatter', String, callback)
-    print('Hello there! I am listening')
+    rospy.init_node('ultimaker_listener', anonymous=True)  # initialize listener node
+    rospy.Subscriber('ultimaker_chatter', String, callback)  # subscribe to topic
+    print('Hello there, I am listening!')
     rospy.spin()  # spin() simply keeps python from exiting until this node is stopped
 
 
 if __name__ == '__main__':
     listener()
-    # callback('run')
+    # callback('s1')
