5a071d24de78757a30d69634a391b92cf4dd4707
Adrian Falke <92855802+ad0409@users.noreply.github.com>
2021-11-07 16:07:34 +0100

    Update ultimaker_support_functions.py

--- ultimaker_support_functions.py
+++ ultimaker_support_functions.py
@@ -5,31 +5,38 @@ import time
 ser = serial.Serial()  # define class
 
 
-def move_printer_x(mm_wanted_x):  # linear moving X, currently not working with negative ints
-    print('Linear moving X ' + mm_wanted_x, ' mm')
-    raw_command_x = str.encode('G0 X ' + str(mm_wanted_x) + ' F0\n')
+def move_printer_x(mm_wanted_x, v_wanted_x):  # linear moving X
+    print('Linear moving X=' + mm_wanted_x + '[mm] ' + 'with Feedrate=' + v_wanted_x + '[mm/min]')
+    raw_command_x = str.encode('F' + str(v_wanted_x) + ' G0 X ' + str(mm_wanted_x) + '\n')
     return raw_command_x
 
 
-def move_printer_y(mm_wanted_y):  # linear moving Y, currently not working with negative ints
-    print('Linear moving Y ' + mm_wanted_y, ' mm')
-    raw_command_y = str.encode('G0 X ' + str(mm_wanted_y) + ' F0\n')
+def move_printer_y(mm_wanted_y, v_wanted_y):  # linear moving Y
+    print('Linear moving Y=' + mm_wanted_y + '[mm] ' + 'with Feedrate=' + v_wanted_y + '[mm/min]')
+    raw_command_y = str.encode('F' + str(v_wanted_y) + ' G0 Y ' + str(mm_wanted_y) + '\n')
     return raw_command_y
 
 
-def move_printer_z(mm_wanted_z):  # linear moving Z, currently not working with negative ints
-    print('Linear moving Z ' + mm_wanted_z, ' mm')
-    raw_command_z = str.encode('G0 X ' + str(mm_wanted_z) + ' F0\n')
+def move_printer_z(mm_wanted_z, v_wanted_z):  # linear moving Z
+    print('Linear moving Z=' + mm_wanted_z + '[mm] ' + 'with Feedrate=' + v_wanted_z + '[mm/min]')
+    raw_command_z = str.encode('F' + str(v_wanted_z) + ' G0 Z ' + str(mm_wanted_z) + '\n')
     return raw_command_z
+
+
+def move_printer_x_y_center():  # centering X,Y
+    print('Centering X,Y with Feedrate=2500[mm/min]')
+    command_x_y_centering = str.encode('F2500 G0 X100.0 Y-100.0 \n')
+    return command_x_y_centering
 
 
 def homing_printer():  # homing X,Y,Z
     print('Homing X,Y,Z')
     command_homing = str.encode('G28\n')
     return command_homing
+    # return ser.write(command_homing)
 
 
-def open_serial_port():
+def disable():
     ser.port = '/dev/ttyACM0'
     ser.baudrate = 250000
     # ser.bytesize =
@@ -39,23 +46,71 @@ def open_serial_port():
     ser.xonxoff = 0
     ser.rtscts = 1
     ser.dsrdtr = 1
-    print('serial is open: ' + str(ser.is_open))
+    ser.open()  # check ser.is_open to see if serial is open
 
+    ser.flush()  # flushing data
+    time.sleep(1)
+    print('Disable steppers')
+    command_disable = str.encode('M84\n')  # M84 = disable steppers
+    ser.write(command_disable)
+    time.sleep(3)
+    command_disable = str.encode('M84\n')  # M84 = disable steppers
+    ser.write(command_disable)
+    
+    
+def printer_off():
     if ser.is_open:
-        print('Serial open... continue')
+        print('Serial already closed.')
     else:
-        print('Serial closed... open it.')
-        ser.open()  # check ser.is_open to see if serial is open
-        print('Wait 1s')
-        time.sleep(1)
+        print('Serial open... close it now.')
+        ser.close()
 
 
-def close_serial_port():
-    if ser.is_open:
-        print('Serial open... closing now.')
-        ser.close()  # check ser.is_open to see if serial is open
-        print('Serial closed.')
-        print('Wait 1s')
-        time.sleep(1)
-    else:
-        print('Serial closed.')
+# def open_serial_port():
+#     if ser.is_open:
+#         print('Serial open... continue')
+#     else:
+#         print('Serial closed... open it.')
+#         ser.port = '/dev/ttyACM0'
+#         ser.baudrate = 250000
+#         # ser.bytesize =
+#         ser.parity = serial.PARITY_EVEN
+#         ser.stopbits = serial.STOPBITS_ONE
+#         ser.timeout = 1
+#         ser.xonxoff = 0
+#         ser.rtscts = 1
+#         ser.dsrdtr = 1
+#         # ser.open()  # check ser.is_open to see if serial is open
+#         print('Wait 1s')
+
+
+# def close_serial_port():
+#     if ser.is_open:
+#         print('Serial open... closing now.')
+#         ser.close()  # check ser.is_open to see if serial is open
+#         print('Serial closed.')
+#         print('Wait 1s')
+#         time.sleep(1)
+#     else:
+#         print('Serial closed.')
+
+
+# def printer_on():
+#     if ser.is_open:
+#         print('Serial open... continue')
+#     else:
+#         print('Serial closed... open it.')
+#         ser.port = '/dev/ttyACM0'
+#         ser.baudrate = 250000
+#         # ser.bytesize =
+#         ser.parity = serial.PARITY_EVEN
+#         ser.stopbits = serial.STOPBITS_ONE
+#         ser.timeout = 1
+#         ser.xonxoff = 0
+#         ser.rtscts = 1
+#         ser.dsrdtr = 1
+#         ser.open()  # check ser.is_open to see if serial is open
+#         ser.flush()  # flushing data
+#         # time.sleep(10)
+#     print('Printer ready.')
+
